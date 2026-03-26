f44c14f2267f3c3c991572a2e11e466bd9042387
thiagoooufmt <tiago.jesus@sou.ufmt.br>
2026-03-26 14:59:48 -0300

    Add files via upload

---
+++ Prog23-03-2026/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+cout << "Bem-vindo ao C++" << endl;
+
+return 0;
+
+}
---
+++ Prog23-03-2026/main2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+cout << "Meu nome é: Thiago" << endl;
+
+return 0;
+
+
+}
