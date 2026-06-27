10a4298b2e8366e21d4e6d7d302fc4f500800501
thiagoooufmt <tiago.jesus@sou.ufmt.br>
2026-06-27 11:04:45 -0300

    Update and rename Prática-Aula 5.txt to main_um.cpp

---
+++ Aula5/main_um.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x = 10;
+    int y = 20;
+
+    cout << "Conteudo de x: " << x << endl;
+    cout << "Endereco de x: " << &x << endl << endl;
+
+    cout << "Conteudo de y: " << y << endl;
+    cout << "Endereco de y: " << &y << endl;
+
+    return 0;
+}
